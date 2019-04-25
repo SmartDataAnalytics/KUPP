9acf2d2c1d5c68672d626bb8f80e049c975b0dfb
mali <ali-mehdi@live.de>
2019-04-25 10:02:33 +0200

    Implement utils to create matrix of literals

---
+++ src/kupp/numeric_literals_preprocessing_utils/basic_utils.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""Utils to process numerical literals."""
+
+import numpy as np
+from typing import Dict
+
+def create_matix_of_literals(triples: np.array, entity_to_id: Dict) -> np.array:
+    """Create matrix of literals where each row corresponds to an entity and each column to a literal."""
+    data_rel_to_id : Dict[str, int] = {
+        value: key
+        for key, value in enumerate(triples[:,2:3])
+    }
+
+    # Prepare literal matrix, set every literal to zero, and afterwards fill in the corresponding value if available
+    num_literals = np.zeros([len(entity_to_id), len(data_rel_to_id)], dtype=np.float32)
+
+    # ToDo: Vecotrize code
+    for i, (h, r, lit) in enumerate(triples):
+        try:
+            # row define entity, and column the literal. Set the corresponding literal for the entity
+            num_literals[entity_to_id[h], data_rel_to_id[r]] = lit
+        except KeyError:
+            continue
+
+    return num_literals

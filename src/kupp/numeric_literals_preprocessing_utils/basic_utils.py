c1329c44a8407576010e271f9cdb5dc18f6dd9f0
Mehdi Ali <ali-mehdi@live.de>
2019-04-29 16:42:59 +0200

    Rename argument

--- src/kupp/numeric_literals_preprocessing_utils/basic_utils.py
+++ src/kupp/numeric_literals_preprocessing_utils/basic_utils.py
@@ -7,18 +7,18 @@ from typing import Dict
 import logging
 log = logging.getLogger(__name__)
 
-def create_matix_of_literals(triples: np.array, entity_to_id: Dict) -> np.array:
+def create_matix_of_literals(numeric_triples: np.array, entity_to_id: Dict) -> np.array:
     """Create matrix of literals where each row corresponds to an entity and each column to a literal."""
     data_rel_to_id : Dict[str, int] = {
         value: key
-        for key, value in enumerate(triples[:,2:3])
+        for key, value in enumerate(numeric_triples[:, 2:3])
     }
 
     # Prepare literal matrix, set every literal to zero, and afterwards fill in the corresponding value if available
     num_literals = np.zeros([len(entity_to_id), len(data_rel_to_id)], dtype=np.float32)
 
     # ToDo: Vecotrize code
-    for i, (h, r, lit) in enumerate(triples):
+    for i, (h, r, lit) in enumerate(numeric_triples):
         try:
             # row define entity, and column the literal. Set the corresponding literal for the entity
             num_literals[entity_to_id[h], data_rel_to_id[r]] = lit

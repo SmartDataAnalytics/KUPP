6f11013152f5fb6390fa79103d02ebd317135ea9
mali <ali-mehdi@live.de>
2019-05-02 10:39:10 +0200

    Fix creation of numeric literals matrix

--- src/kupp/numeric_literals_preprocessing_utils/basic_utils.py
+++ src/kupp/numeric_literals_preprocessing_utils/basic_utils.py
@@ -5,15 +5,17 @@
 import numpy as np
 from typing import Dict
 import logging
+
 log = logging.getLogger(__name__)
+
 
 def create_matix_of_literals(numeric_triples: np.array, entity_to_id: Dict) -> np.ndarray:
     """Create matrix of literals where each row corresponds to an entity and each column to a literal."""
-    data_rel_to_id : Dict[str, int] = {
+    data_relations = np.unique(np.ndarray.flatten(numeric_triples[:, 1:2]))
+    data_rel_to_id: Dict[str, int] = {
         value: key
-        for key, value in enumerate(numeric_triples[:, 2:3])
+        for key, value in enumerate(data_relations)
     }
-
     # Prepare literal matrix, set every literal to zero, and afterwards fill in the corresponding value if available
     num_literals = np.zeros([len(entity_to_id), len(data_rel_to_id)], dtype=np.float32)
 
